0e79007d951e5c78fcc93186ee6519d84e143ca9
Jeff Schwab <jeff@unbuggy.com>
2020-11-14 16:13:29 -0500

    Configure ssh-add in geode-main, not .tmux.conf
    Don't add SSH_AUTH_SOCK to tmux update-environment.  It appears to be
    part of the default value for that option now.  It's probably not what I
    want anyway, because tmux won't pass the variable to new sessions unless
    it was already set in the client environment.  This commit works around
    that by setting the variable in the client environment explicitly before
    connecting to tmux.

--- bin/geode-main.zsh
+++ bin/geode-main.zsh
@@ -3,7 +3,13 @@
 # Geode runs this script by default when it starts (or attaches) a container.
 
 if [[ ! -v SSH_AUTH_SOCK ]]; then
-    eval `ssh-agent` >/dev/null
+    if tmux showenv -g SSH_AUTH_SOCK >& /dev/null; then
+        export $(tmux showenv -g SSH_AUTH_SOCK)
+    else
+        eval `ssh-agent`
+        tmux start
+        tmux setenv -g SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    fi
 fi
 
 exec tmux new -As Geode
