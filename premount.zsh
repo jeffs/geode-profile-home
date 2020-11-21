1296ccee698fee207b9246d7dc78133d0a892f81
Jeff Schwab <jeff@unbuggy.com>
2020-11-21 14:16:10 -0500

    Update premount; Docker bind-mounts are fast again
    They must have just had a glitch; the latest version seems to fix it.

--- premount.zsh
+++ premount.zsh
@@ -3,10 +3,6 @@
 # This script starts a container with a detached process that never dies (until
 # you manually stop it), so that future invocations of `geode run` need not
 # start new containers themselves.
-#
-# Bind-mounting volumes is very slow in my MacBook for some reason.  Launching
-# a new geode-home container takes 6.5s, whereas attaching to an
-# existing one takes only 0.5s.
 
 if docker container inspect geode-home >&/dev/null; then
     echo 'error: container is already running' >&2
@@ -14,4 +10,4 @@ if docker container inspect geode-home >&/dev/null; then
 fi
 
 cd $(dirname $0)
-geode run . perl -MPOSIX -e pause </dev/null &
+geode run . sh -c 'echo premount: Ready; perl -MPOSIX -e pause' </dev/null &
