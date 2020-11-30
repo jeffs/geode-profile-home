260b3cac53515f7c53398a0ce5bfee05c6766bdb
Jeff Schwab <jeff@unbuggy.com>
2020-11-30 14:38:53 -0500

    Install undotree and emmet-vim plugins

--- bin/install-nvim-plugins.zsh
+++ bin/install-nvim-plugins.zsh
@@ -3,5 +3,7 @@
 # Install plugins for Neovim.
 d=~/.local/share/nvim/site/pack/plugins/start
 mkdir -p $d && cd $d
-git clone git@github.com:preservim/nerdtree.git
-git clone git@github.com:cespare/vim-toml.git
+git clone https://github.com/cespare/vim-toml.git
+git clone https://github.com/mattn/emmet-vim.git
+git clone https://github.com/mbbill/undotree.git
+git clone https://github.com/preservim/nerdtree.git
